110a5088b8230e95bd53e67e5edf8d4a78c36d58
SonyesPlay <andres41600@gmail.com>
2022-11-10 21:15:10 -0500

    se refactorizo la clase de mergeNames

--- src/main/java/org/example/MergeNames.java
+++ src/main/java/org/example/MergeNames.java
@@ -5,15 +5,12 @@ import java.util.*;
 public class MergeNames {
 
     public static void main(String[] args) {
-
-        String[] names1 = new String[] {"Ava", "Emma", "Olivia"};
+        String[] names1 = new String[] {"Ava", "Emma", "Emma"};
         String[] names2 = new String[] {"Olivia", "Sophia", "Emma"};
 
-        Set<String> mySet1 = new HashSet<String>(Arrays.asList(names1));
-        Set<String> mySet2 = new HashSet<String>(Arrays.asList(names2));
-
-        Set<String> union = new HashSet<String>(mySet1);
-        union.addAll(mySet2);
+        Set<String> union = new HashSet<>();
+        union.addAll(Arrays.asList(names1));
+        union.addAll(Arrays.asList(names2));
         System.out.println("Union of the two Sets with no duplicate names : " + union);
 
     }
